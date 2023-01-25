5e55b0c1333dcb8c79dad56184d44efa269f4810
kunalkeshan <kunalkeshan12@gmail.com>
2023-01-25 09:12:18 +0530

    chore: added events page - empty template

---
+++ pages/events/[event]/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SingleEventPage = () => {
+  return <div>SingleEventPage</div>;
+};
+
+export default SingleEventPage;
---
+++ pages/events/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EventsPage = () => {
+  return <div>EventsPage</div>;
+};
+
+export default EventsPage;
