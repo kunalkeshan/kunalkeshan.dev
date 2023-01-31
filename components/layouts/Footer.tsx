493d52babf1b67edd4de95f095fb11af3e947aef
kunalkeshan <kunalkeshan12@gmail.com>
2023-01-31 15:27:11 +0530

    chore: setting up footer

--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -1,7 +1,21 @@
+/**
+ * Public Footer Component
+ */
+
+// Dependencies
 import React from "react";
+import { motion } from "framer-motion";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <footer className="mt-4 w-full bg-themes-txt_primary py-9 px-3 text-themes-bg_primary">
+      <motion.div
+        className=""
+        initial={{ opacity: 0, y: 20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+      ></motion.div>
+    </footer>
+  );
 };
 
 export default Footer;
