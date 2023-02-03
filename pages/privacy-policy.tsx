e55cf96d96e9b626d59cd362c53a010cb9e1c4b6
kunalkeshan <kunalkeshan12@gmail.com>
2023-02-03 15:02:59 +0530

    chore: future implementation

---
+++ pages/privacy-policy.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PrivacyPolicyPage = () => {
+  return <div>PrivacyPolicyPage</div>;
+};
+
+export default PrivacyPolicyPage;
---
+++ pages/terms-of-conditions.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TermsOfConditionsPage = () => {
+  return <div>TermsOfConditionsPage</div>;
+};
+
+export default TermsOfConditionsPage;
