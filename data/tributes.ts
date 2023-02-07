93af0e5b959f9099632554f7acd36a4c70d76004
kunalkeshan <kunalkeshan12@gmail.com>
2023-02-07 09:32:03 +0530

    chore: added link if tribute person is featured

--- data/tributes.ts
+++ data/tributes.ts
@@ -20,6 +20,8 @@ interface Tribute {
     website: string;
     logo: string;
   };
+  feature?: boolean;
+  slug?: string;
 }
 
 // Tributes Container
@@ -44,6 +46,8 @@ const TRIBUTES: Tribute[] = [
       website: "https://rentyaar.com",
       logo: "https://res.cloudinary.com/kunalkeshan/image/upload/v1675447141/Portfolio/experience/RENT_YAAR_4_mbi5ek.png",
     },
+    feature: true,
+    slug: "rohan-kumar",
   },
   {
     name: "GS Thina",
@@ -60,6 +64,29 @@ const TRIBUTES: Tribute[] = [
       name: "Think-Digital",
       description: "",
       position: "Founder",
+      website: "https://think-digital.com",
+      logo: "https://res.cloudinary.com/kunalkeshan/image/upload/v1675428847/Portfolio/experience/think_digital_logo_txo2sk.png",
+    },
+    feature: true,
+    slug: "gsthina",
+  },
+  {
+    name: "Raman Shekhawat",
+    website: "https://www.linkedin.com/in/raman-shekhawat-1013381ba/",
+    image:
+      "https://res.cloudinary.com/kunalkeshan/image/upload/v1675741966/Portfolio/tributes/Raman_Shekhawat_yzqbis.jpg",
+    socials: {
+      linkedin: "https://www.linkedin.com/in/raman-shekhawat-1013381ba/",
+      twitter: "",
+      instagram: "",
+      youtube: "",
+    },
+    testimonial:
+      "Having started college life with a friend like Kunal Keshan makes me the perfect person to help you understand him. He is one of the most valuable assets in the developer community. With a dash of hard work and creativity, this fellow has crossed all limits of productivity. Not only will he help you develop your product with perfection, but also with efforts beyond your expectations. Talking about his teamwork, Kunal has an ear for every voice. He is a good listener and believes in empathy. All in all, he is a perfect package you would not want to miss out on while building your team.",
+    company: {
+      name: "Think-Digital",
+      description: "",
+      position: "Web Developer",
       website: "https://think-digital.com",
       logo: "https://res.cloudinary.com/kunalkeshan/image/upload/v1675428847/Portfolio/experience/think_digital_logo_txo2sk.png",
     },
