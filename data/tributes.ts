af2b9b024b9fd3ee3d68db4ef03e353102efd14a
kunalkeshan <kunalkeshan12@gmail.com>
2023-02-07 10:32:26 +0530

    chore: tributes page

--- data/tributes.ts
+++ data/tributes.ts
@@ -7,11 +7,12 @@ interface Tribute {
   website: string;
   image: string;
   testimonial?: string;
-  socials: {
-    linkedin: string;
-    twitter: string;
-    instagram: string;
-    youtube: string;
+  socials?: {
+    linkedin?: string;
+    twitter?: string;
+    instagram?: string;
+    youtube?: string;
+    github?: string;
   };
   company: {
     name: string;
@@ -22,6 +23,7 @@ interface Tribute {
   };
   feature?: boolean;
   slug?: string;
+  intro?: string;
 }
 
 // Tributes Container
@@ -35,8 +37,8 @@ const TRIBUTES: Tribute[] = [
       "Kunal is an innovative and strategic thinker, constantly seeking new and creative solutions to the challenges faced. He has a keen understanding of the market and a strong sense of what it takes to build a successful product. He was able to build a strong and effective team, and his leadership skills inspired and motivated everyone to perform at their best.\n Kunal is a hard worker and always goes the extra mile to ensure that his work is of the highest quality. His commitment to excellence was evident in every project he tackled, and he was always eager to share his knowledge and expertise with others.",
     socials: {
       linkedin: "https://www.linkedin.com/in/kumarrohan9724/",
-      twitter: "",
-      instagram: "",
+      twitter: "https://twitter.com/kumarrohan9724",
+      instagram: "https://www.instagram.com/kumarrohan9724/",
       youtube: "",
     },
     company: {
@@ -48,17 +50,19 @@ const TRIBUTES: Tribute[] = [
     },
     feature: true,
     slug: "rohan-kumar",
+    intro:
+      "Rohan Kumar, a natural leader, charming person, and visionary entrepreneur. Founder of Rentyaar and The Starterseed, changing students' lives for the better.",
   },
   {
     name: "GS Thina",
-    website: "https://www.linkedin.com/in/gsthina/",
+    website: "https://gsthina.me",
     image:
       "https://res.cloudinary.com/kunalkeshan/image/upload/v1675464441/Portfolio/tributes/gs_thina_n80sqg.jpg",
     socials: {
       linkedin: "https://www.linkedin.com/in/gsthina/",
-      twitter: "",
-      instagram: "",
-      youtube: "",
+      twitter: "https://twitter.com/gsthina_me",
+      instagram: "https://www.instagram.com/gsthina.mentor/",
+      youtube: "https://www.youtube.com/@gsthina",
     },
     company: {
       name: "Think-Digital",
@@ -69,18 +73,14 @@ const TRIBUTES: Tribute[] = [
     },
     feature: true,
     slug: "gsthina",
+    intro:
+      "GS Thina, the mentor who empowers college students with skills and industry exposure to reach their full potential 🚀",
   },
   {
     name: "Raman Shekhawat",
     website: "https://www.linkedin.com/in/raman-shekhawat-1013381ba/",
     image:
       "https://res.cloudinary.com/kunalkeshan/image/upload/v1675741966/Portfolio/tributes/Raman_Shekhawat_yzqbis.jpg",
-    socials: {
-      linkedin: "https://www.linkedin.com/in/raman-shekhawat-1013381ba/",
-      twitter: "",
-      instagram: "",
-      youtube: "",
-    },
     testimonial:
       "Having started college life with a friend like Kunal Keshan makes me the perfect person to help you understand him. He is one of the most valuable assets in the developer community. With a dash of hard work and creativity, this fellow has crossed all limits of productivity. Not only will he help you develop your product with perfection, but also with efforts beyond your expectations. Talking about his teamwork, Kunal has an ear for every voice. He is a good listener and believes in empathy. All in all, he is a perfect package you would not want to miss out on while building your team.",
     company: {
--- pages/tributes/index.tsx
+++ pages/tributes/index.tsx
@@ -6,8 +6,122 @@
 import React from "react";
 import Head from "next/head";
 import PublicLayout from "../../layouts/PublicLayout";
-import WorkInProgress from "../../components/reusable/WorkInProgress";
 import { motion } from "framer-motion";
+import TRIBUTES from "../../data/tributes";
+import Link from "next/link";
+import Image from "next/image";
+import {
+  FaInstagram,
+  FaTwitter,
+  FaGlobe,
+  FaLinkedin,
+  FaYoutube,
+} from "react-icons/fa";
+
+const TributeIndividuals = TRIBUTES.filter((tribute) => tribute.feature);
+
+type TributeDataType = typeof TRIBUTES[number];
+
+const TributeCard: React.FC<TributeDataType> = ({
+  slug,
+  name,
+  company,
+  image,
+  intro,
+  socials,
+  website,
+}) => {
+  const socialsInfo = [
+    {
+      name: "Website",
+      Icon: FaGlobe,
+      url: website,
+    },
+    {
+      name: "LinkedIn",
+      Icon: FaLinkedin,
+      url: socials!.linkedin,
+    },
+    {
+      name: "Instagram",
+      Icon: FaInstagram,
+      url: socials!.instagram,
+    },
+    {
+      name: "Twitter",
+      Icon: FaTwitter,
+      url: socials!.twitter,
+    },
+    {
+      name: "YouTube",
+      Icon: FaYoutube,
+      url: socials!.youtube,
+    },
+  ];
+
+  return (
+    <li className="flex w-full flex-col gap-4 rounded-xl border-3 border-black bg-white px-10 py-5 shadow-3d">
+      <div className="relative z-20 mx-auto flex w-full items-center justify-center">
+        <Link
+          href={website}
+          target="_blank"
+          className="flex h-full max-h-[200px] max-w-[200px] items-center justify-center overflow-hidden rounded-full border-2 border-black bg-white shadow-3d-small-reverse transition-all duration-300 hover:-translate-y-1 hover:shadow-3d-reverse"
+        >
+          <Image
+            src={image}
+            width={100}
+            height={100}
+            alt={name}
+            className="h-auto w-full object-contain"
+          ></Image>
+        </Link>
+        <Link
+          href={company.website}
+          target="_blank"
+          className="absolute bottom-0 right-[40px] -z-10 flex h-full max-h-[80px] max-w-[80px] items-center justify-center overflow-hidden rounded-full border-2 border-black bg-white shadow-3d-small transition-all duration-300 hover:z-0 hover:-translate-y-1 hover:shadow-3d"
+        >
+          <Image
+            src={company.logo}
+            width={100}
+            height={100}
+            alt={company.name}
+            className="h-auto w-full object-contain"
+          ></Image>
+        </Link>
+      </div>
+      <h2 className="text-center text-lg font-bold md:text-xl">
+        {name}{" "}
+        <div className="text-base md:text-lg">
+          {company.position} @{company.name}
+        </div>
+      </h2>
+      <p className="mb-8 text-justify font-[Montserrat] text-sm font-semibold md:text-base">
+        {intro}
+      </p>
+      <Link
+        href={`/tributes/${slug}`}
+        className="mt-auto block w-full rounded-lg bg-themes-txt_primary px-8 py-4 text-center font-bold text-themes-bg_primary transition-all duration-300 hover:-translate-y-1 hover:bg-portfolio-accent"
+      >
+        Learn more
+      </Link>
+      <div className="mt-4 flex flex-wrap gap-4">
+        {socialsInfo.map(
+          (social, index) =>
+            social.url && (
+              <Link
+                href={social.url}
+                key={index}
+                target="_blank"
+                className="rounded-full border-2 border-portfolio-accent bg-portfolio-accent bg-opacity-80 p-2 text-xl text-white shadow-3d-small transition-all duration-300 hover:-translate-y-1 hover:bg-opacity-100 hover:shadow-3d"
+              >
+                <social.Icon />
+              </Link>
+            )
+        )}
+      </div>
+    </li>
+  );
+};
 
 const TributesPage = () => {
   return (
@@ -22,7 +136,25 @@ const TributesPage = () => {
           transition={{ delay: 0.4, type: "spring" }}
           className="mx-auto mt-10 mb-20 max-w-7xl px-5"
         >
-          <WorkInProgress />
+          <h1 className="text-center text-3xl font-bold md:text-5xl">
+            My Tribute to those{" "}
+            <span className="bg-portfolio-main px-1 leading-tight">
+              who shaped
+            </span>{" "}
+            who I am today
+          </h1>
+          <p className="mt-4 text-center font-[Montserrat] text-base font-semibold text-themes-txt_secondary md:text-lg">
+            I&apos;ve been fortunate to have had many amazing individuals cross
+            paths with me and play a crucial role in my growth and shaping me
+            into who I am today. This page is my tribute to those who have made
+            a lasting impact on my life and inspired me to keep pushing forward.
+            Join me as I take a walk down memory lane and share my story.
+          </p>
+          <ul className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+            {TributeIndividuals.map((tribute, index) => (
+              <TributeCard key={index} {...tribute} />
+            ))}
+          </ul>
         </motion.section>
       </PublicLayout>
     </>
