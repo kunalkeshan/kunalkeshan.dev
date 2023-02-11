cd3f056ba839f34068519acef4faa5d29bc1e1ab
Kunal Keshan <kunalkeshan12@gmail.com>
2023-02-11 10:31:30 +0530

    chore: viewport meta tag added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         <link
           rel="apple-touch-icon"
