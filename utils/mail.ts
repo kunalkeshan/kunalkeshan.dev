fbcd48306eeb8658f96736deb51850a4e2ab07bb
kunalkeshan <kunalkeshan12@gmail.com>
2023-03-02 02:15:24 +0530

    chore: updated request endpoint method

--- utils/mail.ts
+++ utils/mail.ts
@@ -6,14 +6,14 @@
 import axios from "axios";
 import { isProduction } from "../config";
 
-axios.defaults.baseURL = isProduction
+const requestEndpoint = isProduction
   ? "https://api.kunalkeshan.dev" // Replace this with your production URL
   : "http://localhost:5000"; // Replace this with your local server URL
 
 export const sendContactMail = async (data: PortfolioContactMail) => {
   try {
     const response = await axios.post<{ message: string }>(
-      "/api/contact/portfolio",
+      `${requestEndpoint}/api/contact/portfolio`,
       data
     );
     if (response.status === 200) {
