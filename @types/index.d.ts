2e75c396b0ccc64025a1ba39cdc02cd271913e18
kunalkeshan <kunalkeshan12@gmail.com>
2023-03-02 02:05:18 +0530

    fix: types updated

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,10 +1,5 @@
 import * as IronSession from "iron-session";
 
-type Required<T, K extends keyof T> = T & { [P in K]-?: T[P] };
-
-type WithRequired<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>> &
-  Required<T, K>;
-
 declare module "iron-session" {
   interface IronSessionData {
     user?: {
