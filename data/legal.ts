311bcd169037d42a96825111c1c3e85f0ed3d072
Kunal Keshan <kunalkeshan12@gmail.com>
2023-05-12 21:46:50 +0530

    Chore/Updated privacy policies (#115)

--- data/legal.ts
+++ data/legal.ts
@@ -34,6 +34,11 @@ export const PRIVACY_POLICY: LegalData[] = [
     description:
       "This privacy policy may be updated from time to time to reflect changes in our practices or to comply with legal requirements. Visitors will be notified of any changes by posting the updated policy on the website.",
   },
+  {
+    title: "Contact Us",
+    description:
+      "If you have any questions or concerns about our services or these privacy policies, please don't hesitate to contact us at hey@kunalkeshan.dev.",
+  },
 ];
 
 // Terms of Conditions Container
@@ -42,6 +47,36 @@ export const TERMS_OF_CONDITIONS: LegalData[] = [
     title: "Acceptance of Terms",
     description:
       "By using this website, visitors agree to be bound by these terms of use. If they do not agree to these terms, they should not use the website.",
+  },
+  {
+    title: "Services",
+    description:
+      "We primarily provide web development services. We reserve the right to decline any project that we feel is not a good fit for our team.",
+  },
+  {
+    title: "Pricing",
+    description:
+      "Our pricing for web development services is based on the scope of the project and will be discussed and agreed upon with the client before any work begins.",
+  },
+  {
+    title: "Payment",
+    description:
+      "We require a deposit before beginning any work, with the remaining balance due upon completion of the project. Payment plans can be discussed and agreed upon on a case-by-case basis.",
+  },
+  {
+    title: "Refund Policy",
+    description:
+      "We strive to provide high-quality services to our clients. If for any reason a client is not satisfied with our work, they can request a refund within 7 days of project completion. Refunds will be issued on a prorated basis, based on the work completed.",
+  },
+  {
+    title: "Intellectual Property",
+    description:
+      "All work completed by our team is the intellectual property of our company until full payment has been received by the client. Once full payment has been received, the client will own all intellectual property rights to the work. The content on this website, including but not limited to text, images, and code, is protected by copyright and trademark laws. Visitors may use any content as long as they follow the terms of the Mozilla Public License 2.0.",
+  },
+  {
+    title: "Confidentiality:",
+    description:
+      "We understand the sensitive nature of some projects and will maintain confidentiality regarding all client information and project details.",
   },
   {
     title: "Open Source Nature of Project",
@@ -54,11 +89,6 @@ export const TERMS_OF_CONDITIONS: LegalData[] = [
       "The content on this website is for personal and non-commercial use only. Visitors may use any content for commercial purposes or in their own projects as long as they follow the terms of the Mozilla Public License 2.0.",
   },
   {
-    title: "Intellectual Property",
-    description:
-      "he content on this website, including but not limited to text, images, and code, is protected by copyright and trademark laws. Visitors may use any content as long as they follow the terms of the Mozilla Public License 2.0.",
-  },
-  {
     title: "Disclaimer",
     description:
       'The information on this website is provided on an "as is" basis. No representations or warranties of any kind, express or implied, are made as to the operation of the website or the information, content, materials, or products included on the website.',
@@ -66,11 +96,16 @@ export const TERMS_OF_CONDITIONS: LegalData[] = [
   {
     title: "Limitation of Liability",
     description:
-      "This website will not be liable for any damages of any kind arising from the use of the website or the use of any content by visitors, including but not limited to direct, indirect, incidental, punitive, and consequential damages.",
+      "We reserve the right to revise these terms and conditions from time to time. The most current version will be posted on our website. By using our website or engaging our services, visitors agree to be bound by the then-current version of these terms and conditions. This website will not be liable for any damages of any kind arising from the use of the website or the use of any content by visitors, including but not limited to direct, indirect, incidental, punitive, and consequential damages.",
   },
   {
     title: "Changes to Terms of Use",
     description:
       "These terms of use may be revised from time to time. The most current version will be posted on the website. By using the website, visitors agree to be bound by the then-current version of these terms of use.",
+  },
+  {
+    title: "Contact Us",
+    description:
+      "If you have any questions or concerns about our services or these terms and conditions, please don't hesitate to contact us at hey@kunalkeshan.dev.",
   },
 ];
