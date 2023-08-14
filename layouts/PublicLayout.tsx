3462791e6726507356b6d04265884d7b0a9e71db
kunalkeshan <kunalkeshan12@gmail.com>
2023-08-14 19:35:10 +0530

    fix: remove react-headroom import

--- layouts/PublicLayout.tsx
+++ layouts/PublicLayout.tsx
@@ -7,7 +7,6 @@ import React, { PropsWithChildren } from "react";
 import { NextPage } from "next";
 import Navbar from "../components/layouts/Navbar";
 import Footer from "../components/layouts/Footer";
-import Headroom from "react-headroom";
 
 const PublicLayout: NextPage<PropsWithChildren> = ({ children }) => {
   return (
