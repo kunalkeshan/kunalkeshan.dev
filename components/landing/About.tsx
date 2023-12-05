729c389e7d5f85541a8fe5afb200be311810be97
kunalkeshan <kunalkeshan12@gmail.com>
2023-12-05 19:13:15 +0530

    chore: added unoptimized to about image in landing
    - closes Priority image for About Section - Landing Page #72

--- components/landing/About.tsx
+++ components/landing/About.tsx
@@ -40,6 +40,7 @@ const About = () => {
             width={100}
             height={100}
             className="h-auto w-full object-cover"
+            unoptimized
           />
         </div>
       </div>
