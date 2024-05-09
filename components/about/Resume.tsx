097c3fd59a33673822198eb093adeeb1e1fb981b
Saransh Bangar <saranshbangad@gmail.com>
2024-05-09 09:18:14 +0530

    Updated mb in Resume.tsx

--- components/about/Resume.tsx
+++ components/about/Resume.tsx
@@ -25,29 +25,29 @@ const ResumeCard: React.FC<Experience> = ({
         presentlyWorking ? "shadow-3d hover:shadow-3d-hover" : ""
       } w-full max-w-xs overflow-hidden rounded-xl border-3 border-black transition-all duration-300 md:flex md:max-w-2xl md:flex-row-reverse md:justify-between lg:max-w-4xl`}
     >
-      <div className="w-full items-center justify-between bg-white px-5 py-10 md:flex md:flex-row-reverse">
+      <div className="items-center justify-between w-full px-5 py-10 bg-white md:flex md:flex-row-reverse">
         <Link
           href={website}
           target="_blank"
-          className="flex h-16 w-16 items-center justify-center overflow-hidden rounded-full border-2 border-black shadow-3d-small transition-all duration-300 hover:-translate-y-1 hover:shadow-3d lg:h-20 lg:w-20"
+          className="flex items-center justify-center w-16 h-16 overflow-hidden transition-all duration-300 border-2 border-black rounded-full shadow-3d-small hover:-translate-y-1 hover:shadow-3d lg:h-20 lg:w-20"
         >
           <Image
             src={logo}
             alt={company}
             width={100}
             height={100}
-            className="h-auto w-full object-contain"
+            className="object-contain w-full h-auto"
           />
         </Link>
         <div className="md:max-w-sm lg:max-w-lg">
           <h3 className="mt-4 text-xl font-bold lg:text-2xl">{position},</h3>
           <Link
             href={website}
-            className="text-lg font-semibold underline-offset-2 transition-all duration-300 hover:text-portfolio-accent hover:underline"
+            className="text-lg font-semibold transition-all duration-300 underline-offset-2 hover:text-portfolio-accent hover:underline"
           >
             {company}
           </Link>
-          <p className="mt-4 font-montserrat text-sm font-semibold text-themes-txt_secondary lg:text-base">
+          <p className="mt-4 text-sm font-semibold font-montserrat text-themes-txt_secondary lg:text-base">
             {description}
           </p>
         </div>
@@ -67,12 +67,12 @@ const ResumeCard: React.FC<Experience> = ({
 
 const Resume: React.FC<ResumePageProps> = (props) => {
   return (
-    <div className="mx-auto my-20 max-w-7xl px-5" {...props}>
-      <h2 className="text-center text-3xl font-bold">
+    <div className="px-5 mx-auto my-20 max-w-7xl" {...props}>
+      <h2 className="text-3xl font-bold text-center">
         Take a look at my{" "}
-        <span className="leading-ti bg-portfolio-main px-1">resume</span>
+        <span className="px-1 leading-ti bg-portfolio-main">resume</span>
       </h2>
-      <ul className="mx-auto mt-8 mb-8 flex w-full flex-col items-center justify-center gap-8">
+      <ul className="flex flex-col items-center justify-center w-full gap-8 mx-auto mt-8 mb-16">
         {EXPERIENCE.map((experience, index) => (
           <ResumeCard key={index} {...experience} />
         ))}
